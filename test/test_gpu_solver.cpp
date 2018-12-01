0cbcafbefb58c18f8de6c86dfeafbec914eddd9c
Jake <spacejake@gmail.com>
2018-12-01 15:39:22 +0900

    clarify comment for powells function

--- test/test_gpu_solver.cpp
+++ test/test_gpu_solver.cpp
@@ -103,10 +103,10 @@ TEST_F(PowellTest, solve) {
     //
     //   F = 1/2 (f1^2 + f2^2 + f3^2 + f4^2)
     //
-    //   f1 = x1 + 10*x2;
-    //   f2 = sqrt(5) * (x3 - x4)
-    //   f3 = (x2 - 2*x3)^2
-    //   f4 = sqrt(10) * (x1 - x4)^2
+    //   f1(x1,x2) = x1 + 10*x2;
+    //   f2(x3,x4) = sqrt(5) * (x3 - x4)
+    //   f3(x2,x3) = (x2 - 2*x3)^2
+    //   f4(x1,x4) = sqrt(10) * (x1 - x4)^2
     //
     // The starting values are x1 = 3, x2 = -1, x3 = 0, x4 = 1.
     // The minimum is 0 at (x1, x2, x3, x4) = 0.
